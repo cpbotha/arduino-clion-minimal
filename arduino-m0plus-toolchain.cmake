57dab1ac4170786c1e7623f1b7aed6bea4fffd75
Charl P. Botha <cpbotha@vxlabs.com>
2018-03-24 15:53:19 +0200

    Fix exit_ issues with compiler checks

--- arduino-m0plus-toolchain.cmake
+++ arduino-m0plus-toolchain.cmake
@@ -16,6 +16,10 @@ set(CMAKE_CXX_COMPILER ${ARDUINO_PACKAGES}/arduino/tools/arm-none-eabi-gcc/4.8.3
 SET(COMMON_FLAGS "-mcpu=cortex-m0plus -mthumb -c -g -Os -w -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500 -MMD")
 SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -fno-threadsafe-statics -fno-rtti -fno-exceptions")
 SET(CMAKE_C_FLAGS "${COMMON_FLAGS}")
+# https://stackoverflow.com/questions/19419782/exit-c-text0x18-undefined-reference-to-exit-when-using-arm-none-eabi-gcc
+# https://github.com/vpetrigo/arm-cmake-toolchains/blob/master/arm-gcc-toolchain.cmake
+SET(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=nosys.specs")
+SET(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs")
 
 # BLOODY HELL it's all because it's an ino file and not a cpp ARGH ARGH ARGH don't be so primitve!
 # http://docs.platformio.org/en/latest/faq.html#convert-arduino-file-to-c-manually
